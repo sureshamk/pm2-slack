bc0616c659761a6de6028235ddeb5cf3202b90d1
Suresh <suresh@tricog.com>
2018-08-19 11:50:15 +0530

    just for testing

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 // require('sample')
 
-console.error("Error Happens");
+// console.error("Error Happens");
+console.log("Error Happens");
 console.error(`node-telegram-bot-api deprecated In the future, content-type of files you send will default to "application/octet-stream". See https://github.com/yagop/node-telegram-bot-api/blob/master/doc/usage.md#sending-files for more information on how sending files has been improved and on how to disable this deprecation message altogether. atxxxxxx`);
